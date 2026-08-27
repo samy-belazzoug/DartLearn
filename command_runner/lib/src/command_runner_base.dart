d35b8efbb813af4a940191bbf330f172a54c9fff
samy-belazzoug <samy.belazzoug@pm.me>
2026-08-27 12:36:27 +0200

    Polish CLI app done

--- command_runner/lib/src/command_runner_base.dart
+++ command_runner/lib/src/command_runner_base.dart
@@ -6,21 +6,31 @@ import 'exceptions.dart';
 
 class CommandRunner { // Runs the command line application logic with the given arguments
     
-    // Add a constructor that accepts the optional callback.
-    CommandRunner({this.onError});
+  CommandRunner({this.onOutput, this.onError});
+
+  /// If not null, this method is used to handle output. Useful if you want to
+  /// execute code before the output is printed to the console, or if you
+  /// want to do something other than print output the console.
+  /// If null, the onInput method will [print] the output.
+  FutureOr<void> Function(String)? onOutput;
+
+  FutureOr<void> Function(Object)? onError;
 
   final Map<String, Command> _commands = <String, Command>{};
 
-  UnmodifiableSetView<Command> get command => UnmodifiableSetView<Command>(<Command>{..._commands.values});
-
-  FutureOr<void> Function(Object)? onError;
+  UnmodifiableSetView<Command> get commands => UnmodifiableSetView<Command>(<Command>{..._commands.values});
 
   Future<void> run(List<String> input) async {
       try {
           final ArgResults results = parse(input);
           if (results.command != null) {
             Object? output = await results.command!.run(results);
-            print(output.toString());
+            if (onOutput != null) {
+                await onOutput!(output.toString());
+            } 
+            else {
+                print(output.toString());
+            }
           }
       } on Exception catch (exception) {
           if (onError != null) {
